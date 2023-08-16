b201acd1c8f276142ba4458742915e0af3e845aa
DIMPLE SINGHVI <71957186+DIMPLESINGHVI@users.noreply.github.com>
2023-08-17 01:08:06 +0530

    Update kadanes_optimal.cpp

--- kadanes_optimal.cpp
+++ kadanes_optimal.cpp
@@ -13,11 +13,16 @@ long long maxSubarraySum(int arr[], int n)
     */
     long long sum=0, maxi = LONG_MIN;
     for(int i=0;i<n;i++)
-    {
+    {   // to print the elements of the subarray, we know if the sum is 0 we've got the starting index of the subarray
+        // hence we check here if(sum==0) then start = i; // just the point where the sum was made 0, beg of subarray 
         sum+=arr[i];
         if(sum>maxi)
         {
             maxi = sum;
+            // now this is where we find the max element
+            // so now, ansstart = start; //from where the subarray started
+            // ansend = i; //the point where we found the max sum
+            // now these two indexs can be used to print the array
         }
         if(sum<0)
         {
